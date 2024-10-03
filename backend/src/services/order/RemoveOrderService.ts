1c689bc2559ad5623891517e9b4540490f266ff1
Victor <victor.loures@projetusti.com.br>
2024-10-03 20:19:23 -0300

    Fechando um pedido

---
+++ backend/src/services/order/RemoveOrderService.ts
@@ -0,0 +1,18 @@
+import client from "../../prisma";
+
+interface OrderRequest {
+  order_id: string;
+}
+
+class RemoveOrderService {
+  async execute({ order_id }: OrderRequest) {
+    const order = await client.order.delete({
+      where: {
+        id: order_id,
+      },
+    });
+    return order;
+  }
+}
+
+export { RemoveOrderService };
