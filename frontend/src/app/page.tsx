3796cc56c12d85490c9fbfce4f0fa04c9aadcba1
Victor <victor.loures@projetusti.com.br>
2024-10-08 19:00:25 -0300

    Criando página de login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,39 @@
+import Image from "next/image";
 import styles from "./page.module.scss";
+import logoImg from "/public/logo.svg";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <main>
-      <h1 className={styles.teste}>Login</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo" />
+
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email..."
+              className={styles.input}
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="***********"
+              className={styles.input}
+            />
+
+            <button type="submit">Acessar</button>
+          </form>
+
+          <Link href="/signup" className={styles.text}>
+            Não possui uma conta? Cadastre-se
+          </Link>
+        </section>
+      </div>
+    </>
   );
 }
