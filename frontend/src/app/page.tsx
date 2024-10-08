2866752b5b63e95067ad554964ff0833dd343272
Victor <victor.loures@projetusti.com.br>
2024-10-08 20:47:19 -0300

    Realizando login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,15 +2,38 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import logoImg from "/public/logo.svg";
 import Link from "next/link";
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
 
 export default function Page() {
+  const handleLogin = async (formData: FormData) => {
+    "use server";
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (!email || !password) return;
+
+    try {
+      const response = await api.post("/session", {
+        email,
+        password,
+      });
+      if (!response.data.token) return;
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+
+    redirect("/dashboard");
+  };
+
   return (
     <>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo" />
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}>
             <input
               type="email"
               required
