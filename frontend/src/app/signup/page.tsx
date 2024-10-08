5c734db9ce983fbe1b7e41ef0fa25f4bfa4e1a57
Victor <victor.loures@projetusti.com.br>
2024-10-08 19:15:11 -0300

    Criando signup

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,9 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../page.module.scss";
+import logoImg from "/public/logo.svg";
+
 export default function SignUp() {
   return (
     <>
-      <main>
-        <h1>SignUp</h1>
-      </main>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo" />
+
+        <section className={styles.login}>
+          <h1>Criando sua conta</h1>
+          <form>
+            <input
+              type="text"
+              required
+              name="name"
+              placeholder="Digite seu nome..."
+              className={styles.input}
+            />
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email..."
+              className={styles.input}
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="***********"
+              className={styles.input}
+            />
+
+            <button type="submit">Cadastrar</button>
+          </form>
+
+          <Link href="/" className={styles.text}>
+            Já possui uma conta? Faça o login
+          </Link>
+        </section>
+      </div>
     </>
   );
 }
