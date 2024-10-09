ac2a8cee29f51545846189c0c5ff28a0be9c9971
Victor <victor.loures@projetusti.com.br>
2024-10-08 22:18:52 -0300

    Cadastrando categoria

--- frontend/src/app/dashboard/category/page.tsx
+++ frontend/src/app/dashboard/category/page.tsx
@@ -1,9 +1,32 @@
+import { api } from "@/services/api";
 import { Button } from "../components/button";
 import styles from "./styles.module.scss";
+import { getCookieServer } from "@/lib/cookieServer";
+import { redirect } from "next/navigation";
 
 export default function Category() {
-  const handleRegisterCategory = async () => {
+  const handleRegisterCategory = async (formData: FormData) => {
     "use server";
+    const name = formData.get("name");
+
+    if (!name) return;
+    await api
+      .post(
+        "/category",
+        {
+          name,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${getCookieServer()}`,
+          },
+        }
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+
+    redirect("/dashboard");
   };
 
   return (
