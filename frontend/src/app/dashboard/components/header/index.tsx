1f9bea6c1313c4baa4a962e9c0f62d68d7ee4063
Victor <victor.loures@projetusti.com.br>
2024-10-08 21:45:40 -0300

    Criando dashboard

--- frontend/src/app/dashboard/components/header/index.tsx
+++ frontend/src/app/dashboard/components/header/index.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./styles.module.scss";
 import Image from "next/image";
 import logoImg from "/public/logo.svg";
 import { LogOutIcon } from "lucide-react";
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 export function Header() {
+  const router = useRouter();
+  async function handleLogout() {
+    deleteCookie("session", { path: "/" });
+    router.replace("/");
+  }
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headeContent}>
@@ -22,7 +32,7 @@ export function Header() {
         <nav>
           <Link href="/dashboard/category">Categoria</Link>
           <Link href="/dashboard/product">Produto</Link>
-          <form>
+          <form action={handleLogout}>
             <button type="submit">
               <LogOutIcon size={24} color="#FFF" />
             </button>
