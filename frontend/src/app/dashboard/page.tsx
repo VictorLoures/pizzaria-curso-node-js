4144e086b2aec30ac1cf9b36fbb2e52c440d5474
Victor <victor.loures@projetusti.com.br>
2024-10-09 18:03:04 -0300

    Buscando pedidos

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,6 +1,23 @@
+import { api } from "@/services/api";
 import { Orders } from "./components/orders";
+import { getCookieServer } from "@/lib/cookieServer";
+import { OrderProps } from "@/lib/order.type";
 
-export default function Dashboard() {
+async function getOrders(): Promise<OrderProps[] | []> {
+  const response: any = await api
+    .get("/orders", {
+      headers: {
+        Authorization: `Bearer ${getCookieServer()}`,
+      },
+    })
+    .catch(() => {
+      return [];
+    });
+  return response.data || [];
+}
+
+export default async function Dashboard() {
+  const orders = await getOrders();
   return (
     <>
       <Orders />
