433ecb082b3410d03de9ab0117768b7836cd2d24
Victor <victor.loures@projetusti.com.br>
2024-10-10 23:11:37 -0300

    Ajustando dashboard

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -5,16 +5,24 @@ import { OrderProps } from "@/lib/order.type";
 import { ModalOrder } from "../modal";
 import { use } from "react";
 import { OrderContext } from "@/providers/order";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface Props {
   orders: OrderProps[];
 }
 
 export function Orders({ orders }: Props) {
+  const router = useRouter();
   const { isOpen, onRequestOpen } = use(OrderContext);
 
   async function handleDetailOrder(order_id: string) {
     await onRequestOpen(order_id);
+  }
+
+  function handleRefresh() {
+    router.refresh();
+    toast.success("Pedidos atualizados!");
   }
 
   return (
@@ -22,11 +30,16 @@ export function Orders({ orders }: Props) {
       <main className={styles.container}>
         <section className={styles.containerheader}>
           <h1>Últimos pedidos</h1>
-          <button>
+          <button onClick={handleRefresh}>
             <RefreshCw size={24} color="#3fffa3" />
           </button>
         </section>
         <section className={styles.listOrders}>
+          {orders.length === 0 && (
+            <span className={styles.emptyList}>
+              Nenhum pedido aberto no momento...
+            </span>
+          )}
           {orders.map((it) => (
             <button
               key={it.id}
